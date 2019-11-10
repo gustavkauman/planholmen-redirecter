f4a3851c3cdd3bef1a0e33a0f51143fbb5e8fb1c
Gustav Utke Kauman <gustav.kauman@gmail.com>
2019-11-10 19:00:03 +0100

    Introduce env file

---
+++ env.example.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+define('DB_PREFIX', '');
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+
+define('BASE_URL', '');
