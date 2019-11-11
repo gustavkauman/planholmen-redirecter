bc2aaa0923e7c20656f2d73171a269e307e70231
Gustav Utke Kauman <gustav.kauman@gmail.com>
2019-11-11 14:35:38 +0100

    Order returned rows from MySQL by name

--- public/admin/read.php
+++ public/admin/read.php
@@ -2,7 +2,7 @@
 
 function get_all_urls (&$mysqli) {
 
-    $prep_stmt = 'SELECT * FROM `' . DB_PREFIX . 'redirect`';
+    $prep_stmt = 'SELECT * FROM `' . DB_PREFIX . 'redirect` ORDER BY `name`';
     $stmt = $mysqli->prepare($prep_stmt);
 
     if ( ! $stmt->execute() || ! $res = $stmt->get_result()) {
